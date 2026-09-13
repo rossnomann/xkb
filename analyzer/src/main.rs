563d792ef6d24b118969966d1f00890941c66564
Ross Nomann <rossnomann@protonmail.com>
2026-09-14 01:50:59 +0200

    Use tables in the analyzer output

--- analyzer/src/main.rs
+++ analyzer/src/main.rs
@@ -1,4 +1,3 @@
-use std::borrow::Cow;
 use std::error;
 use std::fmt;
 use std::fs::read_to_string;
@@ -6,6 +5,8 @@ use std::path::Path;
 use std::path::PathBuf;
 
 use clap::Parser;
+use comfy_table::presets::UTF8_FULL;
+use comfy_table::{Row, Table};
 use libdof::Dof;
 use libdof::Keyboard;
 use libdof::dofinitions::Key;
@@ -52,7 +53,7 @@ fn run() -> Result<(), Error> {
     let pins = map_pins(&base_layout, args.pins.as_ref().map_or("", |x| x.as_str()));
     let fast_layout =
         generator.fast_layout(&base_layout, pins.as_ref().map_or(&[], |x| x.as_slice()));
-    report(&generator, &fast_layout);
+    report_layout(&generator, &fast_layout);
     Ok(())
 }
 
@@ -154,86 +155,152 @@ fn map_pins(layout: &Layout, raw: &str) -> Option<Vec<usize>> {
     Some(pins)
 }
 
-fn report(generator: &Oxeylyzer, layout: &FastLayout) {
+fn report_layout(generator: &Oxeylyzer, layout: &FastLayout) {
     print_layout(layout);
 
     let stats = generator.get_layout_stats(&layout);
 
-    println!("Total SFB penalty: {:.3}", stats.sfb);
-    println!(
-        "DSFB penalty (distance 1/2/3): {:.3}/{:.3}/{:.3}",
-        stats.dsfb, stats.dsfb2, stats.dsfb3
-    );
-    println!("Scissors: {:.3}", stats.scissors);
-    println!("LSBS penalty: {:.3}", stats.lsbs);
-    println!("Excessive stretches penalty : {:.3}", stats.stretches);
-    println!("Pinky-ring penalty: {:.3}", stats.pinky_ring);
-    println!("Fspeed penalty: {:.3}", stats.fspeed);
-    println!("Fspeed per finger:");
-    for (finger, fspeed) in stats.finger_speed.iter().enumerate() {
-        println!("\tFinger {finger}: {fspeed:.3}");
-    }
+    let mut table = Table::new();
+    table
+        .load_style(UTF8_FULL)
+        .set_header(["Stats", "", "Trigrams", ""])
+        .add_rows([
+            vrow_f64(
+                "SFB",
+                stats.sfb,
+                "Alternates",
+                stats.trigram_stats.alternates,
+            ),
+            vrow_f64(
+                "DSFB",
+                stats.dsfb,
+                "Alternates SFS",
+                stats.trigram_stats.alternates_sfs,
+            ),
+            vrow_f64("DSFB2", stats.dsfb2, "Inrolls", stats.trigram_stats.inrolls),
+            vrow_f64(
+                "DSFB3",
+                stats.dsfb3,
+                "Outrolls",
+                stats.trigram_stats.outrolls,
+            ),
+            vrow_f64(
+                "Scissors",
+                stats.scissors,
+                "Onehands",
+                stats.trigram_stats.onehands,
+            ),
+            vrow_f64("LSBS", stats.lsbs, "Redirs", stats.trigram_stats.redirects),
+            vrow_f64(
+                "Stretches",
+                stats.stretches,
+                "Redirs SFS",
+                stats.trigram_stats.redirects_sfs,
+            ),
+            vrow_f64(
+                "Pinky ring",
+                stats.pinky_ring,
+                "Bad redirs",
+                stats.trigram_stats.bad_redirects,
+            ),
+            vrow_f64(
+                "Fspeed",
+                stats.fspeed,
+                "Bad redirs SFS",
+                stats.trigram_stats.bad_redirects_sfs,
+            ),
+        ])
+        .add_rows([
+            [
+                "".into(),
+                "".into(),
+                "SFBs".into(),
+                format_f64(stats.trigram_stats.sfbs),
+            ],
+            [
+                "".into(),
+                "".into(),
+                "Bad SFBs".into(),
+                format_f64(stats.trigram_stats.bad_sfbs),
+            ],
+            [
+                "".into(),
+                "".into(),
+                "SFTs".into(),
+                format_f64(stats.trigram_stats.sfts),
+            ],
+            [
+                "".into(),
+                "".into(),
+                "Thumbs".into(),
+                format_f64(stats.trigram_stats.thumbs),
+            ],
+            [
+                "".into(),
+                "".into(),
+                "Other".into(),
+                format_f64(stats.trigram_stats.other),
+            ],
+            [
+                "".into(),
+                "".into(),
+                "Invalid".into(),
+                format_f64(stats.trigram_stats.invalid),
+            ],
+        ])
+        .add_row(["Total score".into(), format!("{}", stats.score)]);
+    println!("{table}");
 
-    println!("Trigrams:");
-    println!("\tAlternates: {:.3}", stats.trigram_stats.alternates);
-    println!(
-        "\tAlternates (same-finger skip): {:.3}",
-        stats.trigram_stats.alternates_sfs
-    );
-    println!("\tInrolls: {:.3}", stats.trigram_stats.inrolls);
-    println!("\tOutrolls: {:.3}", stats.trigram_stats.outrolls);
-    println!("\tOnehands: {:.3}", stats.trigram_stats.onehands);
-    println!("\tRedirects: {:.3}", stats.trigram_stats.redirects);
-    println!(
-        "\tRedirects (same-finger skip): {:.3}",
-        stats.trigram_stats.redirects_sfs
-    );
-    println!("\tBad redirects: {:.3}", stats.trigram_stats.bad_redirects);
-    println!(
-        "\tBad redirects (same-finger skip): {:.3}",
-        stats.trigram_stats.bad_redirects_sfs
-    );
-    println!("\tSFBs: {:.3}", stats.trigram_stats.sfbs);
-    println!("\tBad SFBs: {:.3}", stats.trigram_stats.bad_sfbs);
-    println!("\tSFTs: {:.3}", stats.trigram_stats.sfts);
-    println!("\tThumbs: {:.3}", stats.trigram_stats.thumbs);
-    println!("\tOther: {:.3}", stats.trigram_stats.other);
-    println!("\tInvalid: {:.3}", stats.trigram_stats.invalid);
+    let mut table = Table::new();
+    table.load_style(UTF8_FULL);
+    table.set_header(0..stats.finger_speed.len());
+    table.add_row(stats.finger_speed.iter().map(|x| format_f64(*x)));
+    println!("Fspeed per finger:\n{table}");
+}
 
-    println!("Total score: {}", stats.score);
+fn vrow_f64(label_a: &str, value_a: f64, label_b: &str, value_b: f64) -> Row {
+    [
+        label_a.into(),
+        format_f64(value_a),
+        label_b.into(),
+        format_f64(value_b),
+    ]
+    .into()
+}
+
+fn format_f64(value: f64) -> String {
+    format!("{value:.3}")
 }
 
 fn print_layout(layout: &FastLayout) {
     let mut iter = layout.keys.iter();
     let shape = layout.shape.inner();
     let max_len = shape.iter().max().copied().unwrap_or(0);
+    let mut table = Table::new();
+    table.load_style(UTF8_FULL);
 
     for &l in layout.shape.inner().iter() {
+        let mut cells = vec![];
         let gap = max_len - l;
-        let padding: Cow<str> = if gap > 0 {
-            Cow::Owned(std::iter::repeat(' ').take(gap / 2).collect::<String>())
+        let padding: Vec<char> = if gap > 0 {
+            std::iter::repeat(' ').take(gap / 2).collect()
         } else {
-            Cow::Borrowed("")
+            vec![]
         };
-
+        cells.extend(&padding);
         let mut i = 0;
-        print!("{padding}");
         for u in iter.by_ref() {
             let c = layout.mapping.get_c(*u);
-            print!("{c}");
-
+            cells.push(c);
             i += 1;
-
             if l == i {
                 break;
             }
         }
-        println!("{padding}");
+        cells.extend(padding);
+        table.add_row(cells);
     }
-    println!(
-        "{}",
-        std::iter::repeat('-').take(max_len).collect::<String>()
-    )
+    println!("{table}")
 }
 
 #[derive(Clone, Debug)]
